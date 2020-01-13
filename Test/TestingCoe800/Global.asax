9afe341dc54d4d63678629c3b010b2d989cdff1a
jawadelahmar <49213414+jawadelahmar@users.noreply.github.com>
2020-01-12 21:16:55 -0500

    testing the project

---
+++ Test/TestingCoe800/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestingCoe800.MvcApplication" Language="C#" %>
---
+++ Test/TestingCoe800/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestingCoe800.Startup))]
+namespace TestingCoe800
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
