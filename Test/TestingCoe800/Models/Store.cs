bf9baa29d8ddc34a0a4a1d5f52a8806e72e38118
jawadelahmar <49213414+jawadelahmar@users.noreply.github.com>
2020-03-04 00:04:01 -0500

    testing BLE

--- Test/TestingCoe800/Models/Store.cs
+++ Test/TestingCoe800/Models/Store.cs
@@ -25,7 +25,7 @@ namespace TestingCoe800.Models
         public string Location { get; set; }
         public string ManagerIDFk { get; set; }
         public string PhoneNumber { get; set; }
-        public System.DateTime DateOpened { get; set; }
+        public System.DateTime DateOpened { get; set; } = DateTime.Now;
     
         public virtual AspNetUser AspNetUser { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
